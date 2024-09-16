0008d632f4d49582230013010bf6138946796b9a
Felipe Porceli Volpe <felipe.porceli@outlook.com>
2024-09-16 08:05:17 -0300

    Added new features

--- ec2.sh
+++ ec2.sh
@@ -12,13 +12,25 @@ exibir_menu() {
 
 }
 
+exibir_menu_gruposeguranca () {
+	clear
+        echo " -----------------------------------------"
+        echo "|      MENU GROUP SECURITY EC2 MAKER      |"
+        echo " -----------------------------------------"
+        echo -e "Selecione a opção que deseja em relação aos grupos de segurança das instâncias EC2 da AWS"
+        echo -e "[1] Criar novo grupo de segurança\n[2] Realizar configurações de Firewall\n[3] Consultar informações dos meus grupos\n[4] Voltar para o menu"
+
+}
+
 escolher_opcao() {
 	read -p "Escolha a opção [1-X]: " opcao
 	case $opcao in
 		1)
+			sudo apt install awscli -y
 			versao_aws=$(aws --version)
-			sudo apt install awscli
+			clear
 			echo "Deependencias instaladas com sucesso! A versão atual do AWSCLI é $versao_aws"
+			exibir_menu
 			;;
 		2)
 			aws configure
@@ -28,19 +40,54 @@ escolher_opcao() {
 			read -p "Qual nome deseja dar para a sua chave? " nome_chave
 			aws ec2 create-key-pair --key-name $nome_chave --query 'KeyMaterial' --output text > $nome_chave.pem
 			echo -e "\nPar de chaves criado com sucesso!\nAcesse o arquivo $nome_chave.pem para visualizar"
-		;;
+			;;
 		4)
-			read -p "Qual nome deseja dar ao grupo de segurança? " nome_grupo_seguranca
-			read -p "Por favor, dê uma descrição detalhada para o grupo: " descricao_grupo_seguranca
-			echo "Criando seu grupo..."
-			aws ec2 create-security-group --group-name $nome_grupo_seguranca --description "$descricao_grupo_seguranca" > /dev/null
-			id_grupo=$(aws ec2 describe-security-groups --filters Name=group-name,Values="$nome_grupo_seguranca" --query 'SecurityGroups[0].GroupId' --output text)
-
-			echo -e "Grupo criado com sucesso!\nO Grupo $nome_grupo_seguranca foi criado com o id $id_grupo
-"
-		;;
+			exibir_menu_gruposeguranca
+			escolher_opcao_seguranca
+			;;
 	esac
 }
+
+escolher_opcao_seguranca() {
+	read -p "Escolha a opção [1-4]: " opcao_seguranca
+	case $opcao_seguranca in 
+		1)
+			read -p "Qual nome deseja dar ao grupo de segurança? " nome_grupo_seguranca
+                        read -p "Por favor, dê uma descrição detalhada para o grupo: " descricao_grupo_seguranca
+                        echo "Criando seu grupo..."
+                        aws ec2 create-security-group --group-name $nome_grupo_seguranca --description "$descricao_grupo_seguranca" > /dev/null
+                        id_grupo=$(aws ec2 describe-security-groups --filters Name=group-name,Values="$nome_grupo_seguranca" --query 'SecurityGroups[0].GroupId' --output text)
+
+                        echo -e "Grupo criado com sucesso!\nO Grupo $nome_grupo_seguranca foi criado com o id $id_grupo"
+			sleep 5
+			exibir_menu_gruposeguranca
+			escolher_opcao_seguranca
+			;;
+		2)
+			clear
+			echo "Em construção..."
+			sleep 3
+			exibir_menu_gruposeguranca
+			escolher_opcao_seguranca
+			;;
+		3)
+			clear
+			echo "Em construção..."
+			sleep 3
+			exibir_menu_gruposeguranca
+			escolher_opcao_seguranca
+			;;
+		4)
+			echo "Saindo..."
+			sleep 1
+			exibir_menu
+			escolher_opcao
+			;;
+
+
+	esac
+}
+
 exibir_menu
 escolher_opcao
 
